e2cf3cc1dfb03257f45888979aef5e2631776d09
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-11-15 09:52:31 -0500

    COMP: Update iMSTK and simplify Libusb project available in SlicerIMSTK
    List of iMSTK changes:
    
    $ git shortlog e60770dd5..9b56e096c --no-merges
    Jean-Christophe Fillion-Robin (5):
          DOC: Update incomplete comments in FTD2XX and OpenVR external projects
          COMP: Improve consistency of Libusb external project using imstk_define_external_dirs
          COMP: Simplify Libusb external project removing Libusb_EXTRACT_DIR ivar
          COMP: Update Libusb external project to support reuse from application
          COMP: Update Libusb external project to support Linux

--- SuperBuild/External_Libusb.cmake
+++ SuperBuild/External_Libusb.cmake
@@ -1,90 +1,15 @@
 
 set(proj Libusb)
 
-if(NOT WIN32)
-  set(Slicer_USE_SYSTEM_${proj} TRUE)
+set(${proj}_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
+set(${proj}_INSTALL_DIR ${${proj}_SOURCE_DIR})
+
+include(${iMSTK_SOURCE_DIR}/CMake/External/External_${proj}.cmake)
+if(NOT SB_SECOND_PASS)
+  return()
 endif()
 
-# Set dependency list
-set(${proj}_DEPENDENCIES "")
-
-# Sanity checks
-if(DEFINED ${proj}_ROOT_DIR AND NOT EXISTS ${${proj}_ROOT_DIR})
-  message(FATAL_ERROR "${proj}_ROOT_DIR variable is defined but corresponds to nonexistent directory")
-endif()
-
-if(Slicer_USE_SYSTEM_${proj})
-  unset(Libusb_INCLUDE_DIR CACHE)
-  unset(Libusb_LIBRARY_libusb-1.0-RELEASE CACHE)
-  unset(Libusb_LIBRARY_libusb-1.0-DEBUG CACHE)
-
-  find_package(${proj} REQUIRED)
-endif()
-
-# Include dependent projects if any
-ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
-
-if((NOT DEFINED ${proj}_ROOT_DIR
-   OR NOT DEFINED ${proj}_LIB_DIR) AND NOT Slicer_USE_SYSTEM_${proj})
-
-  set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
-  set(EP_INSTALL_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
-
-  # Set install commands
-  set(libusb_libdir "MS32")
-  if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
-    set(libusb_libdir "MS64")
-  endif()
-
-  set(copy_libusb_headers_command
-    ${CMAKE_COMMAND} -E copy_directory
-    ${EP_SOURCE_DIR}/include
-    ${EP_INSTALL_DIR}/include
-    )
-  set(copy_libusb_lib_command
-    ${CMAKE_COMMAND} -E copy
-    ${EP_SOURCE_DIR}/${libusb_libdir}/dll/libusb-1.0.lib
-    ${EP_INSTALL_DIR}/lib/libusb-1.0.lib
-    )
-  set(copy_libusb_dll_command
-    ${CMAKE_COMMAND} -E copy
-    ${EP_SOURCE_DIR}/${libusb_libdir}/dll/libusb-1.0.dll
-    ${EP_INSTALL_DIR}/bin/
-    )
-
-  ExternalProject_Add(${proj}
-    ${${proj}_EP_ARGS}
-    URL http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.21/libusb-1.0.21.7z https://data.kitware.com/api/v1/item/59cbcefd8d777f7d33e9d9d7/download
-    URL_MD5 7fbcf5580b8ffc88f3af6eddd638de9f
-    DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
-    SOURCE_DIR ${EP_SOURCE_DIR}
-    BINARY_DIR ${EP_BINARY_DIR}
-    UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND
-      COMMAND ${copy_libusb_headers_command}
-      COMMAND ${copy_libusb_lib_command}
-      COMMAND ${copy_libusb_dll_command}
-    DEPENDS
-      ${${proj}_DEPENDENCIES}
-    )
-
-  # TODO: ExternalProject_GenerateProjectDescription_Step
-
-  set(${proj}_ROOT_DIR ${EP_INSTALL_DIR})
-  set(${proj}_LIB_DIR "lib")
-
-  #-----------------------------------------------------------------------------
-  # Launcher setting specific to build tree
-
-  # NA
-
-else()
-  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
-endif()
-
-if(Slicer_USE_SYSTEM_${proj})
+if(USE_SYSTEM_${proj})  # Variable name set in Libusb external project provided by iMSTK
   ExternalProject_Message(${proj} "Libusb_INCLUDE_DIR:${Libusb_INCLUDE_DIR}")
   ExternalProject_Message(${proj} "Libusb_LIBRARY_libusb-1.0-RELEASE:${Libusb_LIBRARY_libusb-1.0-RELEASE}")
   ExternalProject_Message(${proj} "Libusb_LIBRARY_libusb-1.0-DEBUG:${Libusb_LIBRARY_libusb-1.0-DEBUG}")
@@ -98,6 +23,10 @@ if(Slicer_USE_SYSTEM_${proj})
       iMSTK
     )
 else()
+
+  set(${proj}_ROOT_DIR ${${proj}_INSTALL_DIR})
+  set(${proj}_LIB_DIR "lib")
+
   ExternalProject_Message(${proj} "${proj}_ROOT_DIR:${${proj}_ROOT_DIR}")
   ExternalProject_Message(${proj} "${proj}_LIB_DIR:${${proj}_LIB_DIR}")
 
