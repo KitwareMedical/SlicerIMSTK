f5ce02ee02de34d117dff624724f5409d91b91ec
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2021-11-16 13:14:09 -0500

    COMP: Update SuperBuildPrerequisites.cmake to fix configuration in an empty build directory
    Explicitly setting "Slicer_SOURCE_DIR" when only "slicersources_SOURCE_DIR"
    is defined is required to successfully complete configuration in an empty
    build directory.
    
    Indeed, in that case, Slicer sources have been downloaded by they have not been
    added using "add_subdirectory()" and the variable "Slicer_SOURCE_DIR" is not yet in
    in the CACHE.

--- SuperBuildPrerequisites.cmake
+++ SuperBuildPrerequisites.cmake
@@ -1,3 +1,14 @@
+if(DEFINED slicersources_SOURCE_DIR AND NOT DEFINED Slicer_SOURCE_DIR)
+  # Explicitly setting "Slicer_SOURCE_DIR" when only "slicersources_SOURCE_DIR"
+  # is defined is required to successfully complete configuration in an empty
+  # build directory
+  #
+  # Indeed, in that case, Slicer sources have been downloaded by they have not been
+  # added using "add_subdirectory()" and the variable "Slicer_SOURCE_DIR" is not yet in
+  # in the CACHE.
+  set(Slicer_SOURCE_DIR ${slicersources_SOURCE_DIR})
+endif()
+
 if(DEFINED Slicer_SOURCE_DIR)
   # Extension is bundled in a custom application
 
