96f2121f74c9394225a0492fca838a34c4afe0ba
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-02-11 03:55:00 -0500

    COMP: Update iMSTK to fix Assimp project download
    List of changes:
    
    $ git shortlog c55b3ed11..210c0b429 --no-merges
    Jean-Christophe Fillion-Robin (1):
          [Backport MR-770] COMP: Improve source download strategy for Assimp project

--- FetchIMSTK.cmake
+++ FetchIMSTK.cmake
@@ -6,7 +6,7 @@ set(EP_SOURCE_DIR "${CMAKE_BINARY_DIR}/${proj}")
 FetchContent_Populate(${proj}
   SOURCE_DIR     ${EP_SOURCE_DIR}
   GIT_REPOSITORY git://github.com/jcfr/iMSTK.git
-  GIT_TAG        c55b3ed11e1f1cb40f1dcb64e3b6e118e6698bd1  # update-build-system-to-streamline-application-integration
+  GIT_TAG        210c0b4295ba5bcd6c4960f26437d6b74d4e7646  # update-build-system-to-streamline-application-integration
   QUIET
   )
 message(STATUS "Remote - ${proj} [OK]")
