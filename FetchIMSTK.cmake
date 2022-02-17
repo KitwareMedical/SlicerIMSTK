d9584c88fc22be2080fafa08385c8deca511cc32
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-02-17 17:18:55 -0500

    COMP: Download iMSTK from KitwareMedical instead of personal fork
    List of changes specific to the branch "slicerimstk-v5.0.0-2021-12-18-c55b3ed11":
    
    $ git shortlog c55b3ed11..6997eab38 --no-merges
    Jean-Christophe Fillion-Robin (2):
          [Backport MR-770] COMP: Improve source download strategy for Assimp project
          [Backport MR-772] COMP: Update Assimp to simplify packaging in client projects

--- FetchIMSTK.cmake
+++ FetchIMSTK.cmake
@@ -5,8 +5,8 @@ set(proj iMSTK)
 set(EP_SOURCE_DIR "${CMAKE_BINARY_DIR}/${proj}")
 FetchContent_Populate(${proj}
   SOURCE_DIR     ${EP_SOURCE_DIR}
-  GIT_REPOSITORY git://github.com/jcfr/iMSTK.git
-  GIT_TAG        6997eab385ca2eac6c1f192663cde809810f220a  # update-build-system-to-streamline-application-integration
+  GIT_REPOSITORY git://github.com/KitwareMedical/iMSTK.git
+  GIT_TAG        6997eab385ca2eac6c1f192663cde809810f220a  # slicerimstk-v5.0.0-2021-12-18-c55b3ed11
   QUIET
   )
 message(STATUS "Remote - ${proj} [OK]")
