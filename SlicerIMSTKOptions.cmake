823ce6795f8940bb5e42f48b3e7a8111fe949c49
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-06-02 05:50:40 -0400

    COMP: Initialize SlicerIMSTK_BUILD_ViewerVTK to OFF on macOS
    Use of ViewerVTK through the option SlicerIMSTK_BUILD_ViewerVTK in
    either application or extension is currently supported only on Windows
    & Linux because iMSTK ViewerVTK requires OpenVR library not available
    on macOS.
    
    Reorganization of the code is being discussed in https://gitlab.kitware.com/iMSTK/iMSTK/-/issues/432

--- SlicerIMSTKOptions.cmake
+++ SlicerIMSTKOptions.cmake
@@ -1,5 +1,13 @@
 
 if(NOT DEFINED SlicerIMSTK_BUILD_ViewerVTK)
-  option(SlicerIMSTK_BUILD_ViewerVTK "Build iMSTK ViewerVTK" ON)
+  # Since iMSTK ViewerVTK expects OpenVR library that is available only on Linux and Windows,
+  # default value is initialized to OFF for macOS.
+  # See https://gitlab.kitware.com/iMSTK/iMSTK/-/issues/432
+  set(_default ON)
+  if(APPLE)
+    set(_default OFF)
+  endif()
+  message(STATUS "SlicerIMSTK_BUILD_ViewerVTK is not defined. Defaulting to '${_default}'")
+  option(SlicerIMSTK_BUILD_ViewerVTK "Build iMSTK ViewerVTK" ${_default})
 endif()
 mark_as_superbuild(SlicerIMSTK_BUILD_ViewerVTK)
