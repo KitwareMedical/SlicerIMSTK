3948f9874ea1d79432b83dead8be0ac79d358271
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-06-02 03:04:53 -0400

    COMP: Update FetchIMSTK to support setting custom source dir

--- FetchIMSTK.cmake
+++ FetchIMSTK.cmake
@@ -1,16 +1,18 @@
 
 include(FetchContent)
 
-set(proj iMSTK)
-set(EP_SOURCE_DIR "${CMAKE_BINARY_DIR}/${proj}")
-FetchContent_Populate(${proj}
-  SOURCE_DIR     ${EP_SOURCE_DIR}
-  GIT_REPOSITORY ${EP_GIT_PROTOCOL}://github.com/KitwareMedical/iMSTK.git
-  GIT_TAG        c1e1f758c03b051edf27fa9223e5df5a7404d938  # slicerimstk-v5.0.0-2022-05-26-beb83349
-  QUIET
-  )
-message(STATUS "Remote - ${proj} [OK]")
+if(NOT DEFINED iMSTK_SOURCE_DIR)
+  set(proj iMSTK)
+  set(EP_SOURCE_DIR "${CMAKE_BINARY_DIR}/${proj}")
+  FetchContent_Populate(${proj}
+    SOURCE_DIR     ${EP_SOURCE_DIR}
+    GIT_REPOSITORY ${EP_GIT_PROTOCOL}://github.com/KitwareMedical/iMSTK.git
+    GIT_TAG        c1e1f758c03b051edf27fa9223e5df5a7404d938  # slicerimstk-v5.0.0-2022-05-26-beb83349
+    QUIET
+    )
+  message(STATUS "Remote - ${proj} [OK]")
 
-set(iMSTK_SOURCE_DIR ${EP_SOURCE_DIR})
+  set(iMSTK_SOURCE_DIR ${EP_SOURCE_DIR})
+endif()
 message(STATUS "Remote - iMSTK_SOURCE_DIR:${iMSTK_SOURCE_DIR}")
 
